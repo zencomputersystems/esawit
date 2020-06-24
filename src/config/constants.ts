a03927ab24061ebcb192e3f29863e502f184ac4d
steelburn <steelburn@gmail.com>
2020-06-25 03:21:41 +0800

    Changed HTTP to HTTPS

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
 export const DREAMFACTORY_API_KEY: string = 'b34c8b6e26a41f07dee48513714a534920f647cd48f299e9f28410a86d8a2cb4';
-export const DREAMFACTORY_TABLE_URL: string = 'http://api.zen.com.my/api/v2/esawitdb/_table';
+export const DREAMFACTORY_TABLE_URL: string = 'https://api.zen.com.my/api/v2/esawitdb/_table';
